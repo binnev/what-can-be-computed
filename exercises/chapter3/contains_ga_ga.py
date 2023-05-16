515f8198730188a901d1b157fa924301bcbcd407
binnev <byntje@gmail.com>
2023-05-16 13:36:38 +0200

    Added test for 3.3

---
+++ exercises/chapter3/contains_ga_ga.py
@@ -0,0 +1,5 @@
+def containsGA_GA(inString):
+    if "GA" * 2 in inString:
+        return "yes"
+    else:
+        return "no"
--- exercises/test_chapter3.py
+++ exercises/test_chapter3.py
@@ -1,6 +1,8 @@
 import pytest
 
 import utils
+from containsGAGA import containsGAGA
+from exercises.chapter3.contains_ga_ga import containsGA_GA
 from exercises.chapter3.count_chars import count_chars
 from exercises.chapter3.count_chars_b import count_chars_b
 
@@ -20,5 +22,18 @@ def test_3_2(func, input, solution):
     because their own source code is different lengths, obviously. Both programs give the same
     answer to all the inputs.
     """
-    val = func(input)
-    assert val == solution
+    assert func(input) == solution
+
+
+@pytest.mark.parametrize("func", [containsGAGA, containsGA_GA])
+@pytest.mark.parametrize(
+    "input, solution",
+    [
+        ("CATTG", "no"),
+        ("GAGA", "yes"),
+        ("GA_GA", "no"),
+        (utils.rf("exercises/chapter3/contains_ga_ga.py"), "no"),
+    ],
+)
+def test_3_3(func, input, solution):
+    assert func(input) == solution
