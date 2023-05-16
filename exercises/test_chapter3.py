40ac9458d3cd8bdd0d5eee0aca22190c300b3f20
binnev <byntje@gmail.com>
2023-05-16 13:45:09 +0200

    Added test for 3.4

--- exercises/test_chapter3.py
+++ exercises/test_chapter3.py
@@ -1,10 +1,13 @@
 import pytest
 
-import utils
 from containsGAGA import containsGAGA
 from exercises.chapter3.contains_ga_ga import containsGA_GA
 from exercises.chapter3.count_chars import count_chars
 from exercises.chapter3.count_chars_b import count_chars_b
+from notYesOnSelfApprox import notYesOnSelfApprox
+from utils import rf
+from yesOnSelfApprox import yesOnSelfApprox
+from yesOnStringApprox import yesOnStringApprox
 
 
 @pytest.mark.parametrize("func", [count_chars, count_chars_b])
@@ -12,8 +15,8 @@ from exercises.chapter3.count_chars_b import count_chars_b
     "input, solution",
     [
         ("CATTG", 5),
-        (utils.rf("exercises/chapter3/count_chars.py"), 58),
-        (utils.rf("exercises/chapter3/count_chars_b.py"), 103),
+        (rf("exercises/chapter3/count_chars.py"), 58),
+        (rf("exercises/chapter3/count_chars_b.py"), 103),
     ],
 )
 def test_3_2(func, input, solution):
@@ -32,8 +35,21 @@ def test_3_2(func, input, solution):
         ("CATTG", "no"),
         ("GAGA", "yes"),
         ("GA_GA", "no"),
-        (utils.rf("exercises/chapter3/contains_ga_ga.py"), "no"),
+        (rf("exercises/chapter3/contains_ga_ga.py"), "no"),
     ],
 )
 def test_3_3(func, input, solution):
     assert func(input) == solution
+
+
+@pytest.mark.parametrize(
+    "output, expected",
+    [
+        (yesOnStringApprox(rf("longerThan1K.py"), rf("longerThan1K.py")), "no"),
+        (yesOnStringApprox(rf("maybeLoop.py"), rf("maybeLoop.py")), "yes"),
+        (yesOnSelfApprox(rf("longerThan1K.py")), "no"),
+        (notYesOnSelfApprox(rf("containsGAGA.py")), "no"),
+    ],
+)
+def test_3_4(output, expected):
+    assert output == expected
